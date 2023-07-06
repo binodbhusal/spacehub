d096b807b11d392b90f1d15e74b3e16099daa4e0
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-07-06 15:48:50 +0100

    add rocket/mission component

---
+++ src/Redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rocketReducer from './rocketSlice';
+import missionReducer from './missionSlice';
+
+const store = configureStore({
+    reducer: {
+        rocket: rocketReducer,
+        mission: missionReducer,
+    }
+})
+    
+export default store;
